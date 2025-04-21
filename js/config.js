9202e1305e1aa4c8da6e00959a8c115da5bc52bd
Aditya Pakala <87964872+adityapakala@users.noreply.github.com>
2025-04-20 22:21:47 -0500

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'us-east-1_cL61r41av', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '3s5l0ka8ioe68evv5e0t7h0jfq', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-east-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
